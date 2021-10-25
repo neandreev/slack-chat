7b458cca09de0c6406e874b68087d6dbde0616c3
Andreev Danila <mail@neandreev.ru>
2021-10-25 16:50:00 +0500

    update i18n config

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,4 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import ru from './locales/ru.js';
@@ -9,9 +8,8 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'ru',
-    lng: 'ru',
-    debug: true,
+    fallbackLng: 'en',
+    debug: false,
     resources: { ru, en },
   });
 
