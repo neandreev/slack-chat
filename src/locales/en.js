16c981e0c585733f21d7198a70e6240c40370700
Andreev Danila <mail@neandreev.ru>
2021-10-26 14:39:14 +0500

    eslint locales

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,14 +1,14 @@
 export default {
   translation: {
-    navigation:{
-      logout: 'Logout'
+    navigation: {
+      logout: 'Logout',
     },
     loginForm: {
-      login: "Login",
-      password: "Password",
-      submit: "Login",
-      signup: "Signup",
-      header: "Welcome to chat",
+      login: 'Login',
+      password: 'Password',
+      submit: 'Login',
+      signup: 'Signup',
+      header: 'Welcome to chat',
       unauthorized: 'Wrong login or password',
       required: 'This field is required',
       loginLimit: 'From 3 to 20 symbols',
@@ -16,37 +16,37 @@ export default {
       noAccount: 'No account?',
     },
     signupForm: {
-      login: "Login",
-      password: "Password",
-      passwordConfirmation: "Password confirmation",
-      submit: "Sign up",
-      header: "Sign up",
+      login: 'Login',
+      password: 'Password',
+      passwordConfirmation: 'Password confirmation',
+      submit: 'Sign up',
+      header: 'Sign up',
       conflict: 'This user already exists!',
       loginLimit: 'From 3 to 20 symbols',
       passwordLimit: 'From 6 symbols',
-      passwordsNotMatch: 'Passwords not match'
+      passwordsNotMatch: 'Passwords not match',
     },
     chat: {
-      channels: "channels",
-      messages: "Messages",
-      addChannel: "Add channel",
-      noMessages: "No messages here... Be the first!",
-      messagePlaceholder: "Your message...",
-      submit: "Send",
-      rename: "Rename",
-      remove: "Remove",
-      confirmRemove: 'Are you sure?'
+      channels: 'channels',
+      messages: 'Messages',
+      addChannel: 'Add channel',
+      noMessages: 'No messages here... Be the first!',
+      messagePlaceholder: 'Your message...',
+      submit: 'Send',
+      rename: 'Rename',
+      remove: 'Remove',
+      confirmRemove: 'Are you sure?',
     },
     modal: {
-      renameTitle: "Rename channel",
-      removeTitle: "Remove channel",
-      addTitle: "Add channel",
-      close: "Close",
+      renameTitle: 'Rename channel',
+      removeTitle: 'Remove channel',
+      addTitle: 'Add channel',
+      close: 'Close',
       submit: {
-        add: "Add",
-        remove: "Remove",
-        rename: "Rename"
-      }
-    }
-  }
+        add: 'Add',
+        remove: 'Remove',
+        rename: 'Rename',
+      },
+    },
+  },
 };
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,14 +1,14 @@
 export default {
   translation: {
-    navigation:{
-      logout: 'Выйти'
+    navigation: {
+      logout: 'Выйти',
     },
     loginForm: {
-      login: "Ваш ник",
-      password: "Пароль",
-      submit: "Войти",
-      signup: "Регистрация",
-      header: "Добро пожаловать в чат",
+      login: 'Ваш ник',
+      password: 'Пароль',
+      submit: 'Войти',
+      signup: 'Регистрация',
+      header: 'Добро пожаловать в чат',
       unauthorized: 'Неверные имя пользователя или пароль',
       required: 'Обязательное поле',
       loginLimit: 'От 3 до 20 символов',
@@ -16,38 +16,38 @@ export default {
       noAccount: 'Нет аккаунта?',
     },
     signupForm: {
-      login: "Ваш ник",
-      password: "Пароль",
-      passwordConfirmation: "Подтвердите пароль",
-      submit: "Зарегистрироваться",
-      header: "Регистрация",
+      login: 'Ваш ник',
+      password: 'Пароль',
+      passwordConfirmation: 'Подтвердите пароль',
+      submit: 'Зарегистрироваться',
+      header: 'Регистрация',
       conflict: 'Данный пользователь уже существует!',
       required: 'Обязательное поле',
       loginLimit: 'От 3 до 20 символов',
       passwordLimit: 'Не менее 6 символов',
-      passwordsNotMatch: 'Пароли должны совпадать'
+      passwordsNotMatch: 'Пароли должны совпадать',
     },
     chat: {
-      channels: "Каналы",
-      messages: "Сообщения",
-      addChannel: "Добавить канал",
-      noMessages: "Пока сообщений нет... Будьте первыми!",
-      messagePlaceholder: "Ваше сообщение...",
-      submit: "Отправить",
-      rename: "Переименовать",
-      remove: "Удалить",
-      confirmRemove: 'Вы уверены?'
+      channels: 'Каналы',
+      messages: 'Сообщения',
+      addChannel: 'Добавить канал',
+      noMessages: 'Пока сообщений нет... Будьте первыми!',
+      messagePlaceholder: 'Ваше сообщение...',
+      submit: 'Отправить',
+      rename: 'Переименовать',
+      remove: 'Удалить',
+      confirmRemove: 'Вы уверены?',
     },
     modal: {
-      renameTitle: "Переименовать канал",
-      removeTitle: "Удалить канал",
-      addTitle: "Добавить канал",
-      close: "Закрыть",
+      renameTitle: 'Переименовать канал',
+      removeTitle: 'Удалить канал',
+      addTitle: 'Добавить канал',
+      close: 'Закрыть',
       submit: {
-        add: "Добавить",
-        remove: "Удалить",
-        rename: "Переименовать"
-      }
-    }
-  }
+        add: 'Добавить',
+        remove: 'Удалить',
+        rename: 'Переименовать',
+      },
+    },
+  },
 };
