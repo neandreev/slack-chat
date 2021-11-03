9525f674345894a856f2e8a4c08f6e7cd6e1db9c
Andreev Danila <mail@neandreev.ru>
2021-11-03 14:39:59 +0500

    update package main file

---
+++ src/bundledApp.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import ProvideSocket from './context/ProvideSocket.jsx';
+import ProvideAuth from './context/ProvideAuth.jsx';
+import rootReducer from './redux/rootReducer.js';
+import App from './App.jsx';
+import i18n from './i18n';
+
+import '../assets/application.scss';
+
+const store = configureStore({ reducer: rootReducer });
+
+const rollbarConfig = {
+  accessToken: '713df683ca1a45038b1c66b5ebd734fa',
+  environment: 'development',
+  enabled: true,
+};
+
+const app = (
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <I18nextProvider i18n={i18n}>
+          <ProvideAuth>
+            <ProvideSocket>
+              <App />
+            </ProvideSocket>
+          </ProvideAuth>
+        </I18nextProvider>
+      </Provider>
+    </ErrorBoundary>
+  </RollbarProvider>
+);
+
+export default app;
--- src/index.jsx
+++ src/index.jsx
@@ -1,48 +1,11 @@
 // @ts-check
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { I18nextProvider } from 'react-i18next';
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import bundledApp from './bundledApp';
 import 'core-js/stable/index';
 import 'regenerator-runtime/runtime';
-import i18n from './i18n';
-import App from './App.jsx';
-import ProvideAuth from './context/ProvideAuth.jsx';
-import ProvideSocket from './context/ProvideSocket.jsx';
-
-import '../assets/application.scss';
-import rootReducer from './redux/rootReducer.js';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const rollbarConfig = {
-  accessToken: '713df683ca1a45038b1c66b5ebd734fa',
-  environment: 'development',
-  enabled: true,
-};
-
-const store = configureStore({ reducer: rootReducer });
-
-const app = (
-  <RollbarProvider config={rollbarConfig}>
-    <ErrorBoundary>
-      <Provider store={store}>
-        <I18nextProvider i18n={i18n}>
-          <ProvideAuth>
-            <ProvideSocket>
-              <App />
-            </ProvideSocket>
-          </ProvideAuth>
-        </I18nextProvider>
-      </Provider>
-    </ErrorBoundary>
-  </RollbarProvider>
-);
-
-ReactDOM.render(app, document.querySelector('#chat'));
-
-export default app;
+ReactDOM.render(bundledApp, document.querySelector('#chat'));
